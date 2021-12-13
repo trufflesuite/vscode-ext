2975d1bb5d15f2f38e7f78939da79ed4105f3509
MB <951378+michaeljohnbennett@users.noreply.github.com>
2021-12-13 19:58:14 +1100

    fixing the target/view names.

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Consensys Software Inc. All rights reserved.
 // Licensed under the MIT license.
 
-import { commands, ExtensionContext, Uri, window, workspace } from "vscode";
+import {commands, ExtensionContext, Uri, window, workspace} from "vscode";
 import {
   DebuggerCommands,
   GanacheCommands,
@@ -12,11 +12,11 @@ import {
   ServiceCommands,
   TruffleCommands,
 } from "./commands";
-import { Constants } from "./Constants";
-import { CommandContext, isWorkspaceOpen, openZeppelinHelper, required, setCommandContext } from "./helpers";
-import { CancellationEvent } from "./Models";
-import { Output } from "./Output";
-import { ChangelogPage, RequirementsPage, WelcomePage } from "./pages";
+import {Constants} from "./Constants";
+import {CommandContext, isWorkspaceOpen, openZeppelinHelper, required, setCommandContext} from "./helpers";
+import {CancellationEvent} from "./Models";
+import {Output} from "./Output";
+import {ChangelogPage, RequirementsPage, WelcomePage} from "./pages";
 import {
   AdapterType,
   ContractDB,
@@ -26,10 +26,10 @@ import {
   TreeManager,
   TreeService,
 } from "./services";
-import { Telemetry } from "./TelemetryClient";
-import { NetworkNodeView, ProjectView } from "./ViewItems";
+import {Telemetry} from "./TelemetryClient";
+import {NetworkNodeView, ProjectView} from "./ViewItems";
 
-import { DebuggerConfiguration } from "./debugAdapter/configuration/debuggerConfiguration";
+import {DebuggerConfiguration} from "./debugAdapter/configuration/debuggerConfiguration";
 
 export async function activate(context: ExtensionContext) {
   if (process.env.CODE_TEST) {
@@ -43,7 +43,7 @@ export async function activate(context: ExtensionContext) {
   MnemonicRepository.initialize(context.globalState);
   TreeManager.initialize(context.globalState);
   // FIXME: i think this is breaking things..
-  TreeService.initialize("AzureBlockchain");
+  TreeService.initialize("Trufflesuite");
   await sdkCoreCommands.initialize(context.globalState);
 
   setCommandContext(CommandContext.Enabled, true);
