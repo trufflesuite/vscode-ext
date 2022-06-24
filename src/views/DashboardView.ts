7abbf6682b6b043dcd33294b6630861c7648ed6a
xhulz <wpmwsc@gmail.com>
2022-06-24 17:36:28 +0100

    feat: Tree view implementation has changed from AZTreeView to TreeView

--- src/views/DashboardView.ts
+++ src/views/DashboardView.ts
@@ -1,37 +1,59 @@
-import {
-  AzExtParentTreeItem,
-  AzExtTreeDataProvider,
-  AzExtTreeItem,
-  GenericTreeItem,
-  IActionContext,
-} from "@microsoft/vscode-azext-utils";
-import vscode, {commands} from "vscode";
+import vscode, {
+  commands,
+  ProviderResult,
+  ThemeIcon,
+  TreeItem,
+  Uri,
+  Event,
+  TreeDataProvider,
+  TreeItemCollapsibleState,
+  TreeView,
+} from "vscode";
 import {DashboardCommands} from "../commands";
 import {Constants} from "../Constants";
 import {vscodeEnvironment} from "../helpers";
 
-export class DashboardViewTreeItem extends AzExtParentTreeItem {
-  public constructor(parent: AzExtParentTreeItem | undefined) {
-    super(parent);
-  }
-  public async loadMoreChildrenImpl(_clearCache: boolean, _context: IActionContext): Promise<AzExtTreeItem[]> {
-    return [
-      new GenericTreeItem(this, {
-        label: this.label,
-        contextValue: this.contextValue,
-        description: `${Constants.networkProtocols.http}${Constants.localhost}:${Constants.dashboardPort}`,
-        iconPath: Constants.treeItemData.service.dashboard.iconPath,
-      }),
+class DashboardTreeDataProvider implements TreeDataProvider<DashboardTreeItem> {
+  onDidChangeTreeData?: Event<void | DashboardTreeItem | DashboardTreeItem[] | null | undefined> | undefined;
+
+  parent: DashboardTreeItem[];
+
+  constructor() {
+    this.parent = [
+      new DashboardTreeItem(
+        Constants.treeItemData.service.dashboard.label,
+        `${Constants.networkProtocols.http}${Constants.localhost}:${Constants.dashboardPort}`,
+        Constants.treeItemData.service.dashboard.iconPath,
+        Constants.treeItemData.service.dashboard.contextValue
+      ),
     ];
   }
-  public hasMoreChildrenImpl(): boolean {
-    return false;
+
+  getTreeItem(element: DashboardTreeItem): TreeItem | Thenable<TreeItem> {
+    return element;
   }
-  async refreshImpl(_: IActionContext): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  getChildren(element?: DashboardTreeItem | undefined): ProviderResult<DashboardTreeItem[]> {
+    if (element === undefined) {
+      return this.parent;
+    }
+    return element.children;
   }
-  public label: string = Constants.treeItemData.service.dashboard.label;
-  public contextValue: string = Constants.treeItemData.service.dashboard.contextValue;
+}
+
+class DashboardTreeItem extends TreeItem {
+  constructor(
+    readonly label: string,
+    readonly description: string,
+    readonly iconPath: string | Uri | {light: string | Uri; dark: string | Uri} | ThemeIcon,
+    readonly contextValue: string,
+    readonly children?: DashboardTreeItem[]
+  ) {
+    super(label, children === undefined ? TreeItemCollapsibleState.None : TreeItemCollapsibleState.Expanded);
+    this.description = description;
+    this.iconPath = iconPath;
+    this.contextValue = contextValue;
+  }
 }
 
 /**
@@ -41,12 +63,7 @@ export class DashboardViewTreeItem extends AzExtParentTreeItem {
  *
  * @param viewId - the viewId - defaults to above.
  */
-export function registerDashboardView(
-  viewId: string = "truffle-vscode.views.dashboard"
-): vscode.TreeView<AzExtTreeItem> {
-  const root = new DashboardViewTreeItem(undefined);
-  const treeDataProvider = new AzExtTreeDataProvider(root, `${viewId}.loadMore`);
-
+export function registerDashboardView(viewId: string = "truffle-vscode.views.dashboard"): TreeView<DashboardTreeItem> {
   commands.registerCommand(`${viewId}.startDashboardServer`, async () => {
     await DashboardCommands.startDashboardCmd();
   });
@@ -57,11 +74,13 @@ export function registerDashboardView(
     await DashboardCommands.stopDashboardCmd().then(() => DashboardCommands.startDashboardCmd());
   });
   commands.registerCommand(`${viewId}.copyRPCEndpointAddress`, async () => {
-    const rpc = `${Constants.networkProtocols.http}${Constants.localhost}:${Constants.dashboardPort}/rpc`;
-
-    await vscodeEnvironment.writeToClipboard(rpc);
+    await vscodeEnvironment.writeToClipboard(
+      `${Constants.networkProtocols.http}${Constants.localhost}:${Constants.dashboardPort}/rpc`
+    );
     vscode.window.showInformationMessage(Constants.informationMessage.rpcEndpointCopiedToClipboard);
   });
 
-  return vscode.window.createTreeView(viewId, {treeDataProvider, canSelectMany: true});
+  return vscode.window.createTreeView("truffle-vscode.views.dashboard", {
+    treeDataProvider: new DashboardTreeDataProvider(),
+  });
 }
