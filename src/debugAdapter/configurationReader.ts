05ff286a06d2c878d7e1bf1edd89d2037c259984
MB <951378+michaeljohnbennett@users.noreply.github.com>
2022-06-30 14:43:37 +1000

    chore: adding in deleted file.

---
+++ src/debugAdapter/configurationReader.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Consensys Software Inc. All rights reserved.
+// Licensed under the MIT license.
+
+// The same implementation as in helpers/truffleConfig.ts
+// The difference is that all code which uses 'vscode' module is removed.
+// TODO: think how to reuse code
+
+export namespace ConfigurationReader {
+  // EMPTY NOW
+}
