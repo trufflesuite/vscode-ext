118fd1ceaf40994a07a0872a10f288d598f3a2cf
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-11-25 12:32:34 +0100

    fix: try CI with increasing memory

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'eval',
+  devtool: false,
   optimization: {
     minimize: true,
   },
