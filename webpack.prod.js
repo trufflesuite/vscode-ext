f06cf77cc4ff2c2bbf53b884bc9e76f06c7a565a
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-11-30 10:27:39 -0300

    docs: add comment about `devtool` option

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,8 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  // By removing source map generation, we reduce bundle size by 50%.
+  // See https://webpack.js.org/configuration/devtool/ for more details.
   devtool: false,
   optimization: {
     minimize: true,
