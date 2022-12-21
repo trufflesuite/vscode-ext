5065cef01bbadadcffffcb4ebb21c89a40ab0b6f
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-12-21 20:53:41 -0300

    fix: use new telemetry package

--- src/TelemetryClient.ts
+++ src/TelemetryClient.ts
@@ -3,51 +3,43 @@
 
 import crypto from 'crypto';
 import os from 'os';
-import {OutputChannel, window, workspace} from 'vscode';
-import TelemetryReporter from 'vscode-extension-telemetry';
+import {workspace} from 'vscode';
+import TelemetryReporter, {
+  type TelemetryEventMeasurements,
+  type TelemetryEventProperties,
+} from '@vscode/extension-telemetry';
 import {Constants} from './Constants';
 import {Output, OutputLabel} from './Output';
 
 class ExtensionTelemetry {
-  private readonly output?: OutputChannel;
-
   private readonly reporter?: TelemetryReporter;
 
   private readonly defaultProperties: {[key: string]: any} = {};
 
   constructor() {
     const isEnableTelemetry = workspace.getConfiguration('telemetry').get('enableTelemetry') || true;
-    const isTestRun = process.env.CODE_TEST || false;
     // make testing easier.
     const extensionKey = process.env.AIKEY || Constants.extensionKey;
     if (isEnableTelemetry) {
-      if (isTestRun) {
-        this.output = window.createOutputChannel(OutputLabel.telemetryClient);
-      } else {
-        try {
-          this.reporter = new TelemetryReporter(Constants.extensionName, Constants.extensionVersion, extensionKey);
-          // set default values for machine/session ids
-          this.defaultProperties['common.vscodemachineid'] = generateMachineId();
-          this.defaultProperties['common.vscodesessionid'] = generateSessionId();
-        } catch (error) {
-          Output.outputLine(OutputLabel.telemetryClient, `Initialize done with error: ${(error as Error).message}`);
-        }
+      try {
+        this.reporter = new TelemetryReporter(extensionKey);
+        // set default values for machine/session ids
+        this.defaultProperties['common.vscodemachineid'] = generateMachineId();
+        this.defaultProperties['common.vscodesessionid'] = generateSessionId();
+      } catch (error) {
+        Output.outputLine(OutputLabel.telemetryClient, `Initialize done with error: ${(error as Error).message}`);
       }
     }
   }
 
   public sendEvent(
     eventName: string,
-    properties?: {[key: string]: string},
-    measurements?: {[key: string]: number}
+    properties?: TelemetryEventProperties,
+    measurements?: TelemetryEventMeasurements
   ): void {
     const props = {...this.defaultProperties, ...properties};
     if (this.reporter) {
       this.reporter.sendTelemetryEvent(eventName, props, measurements);
-    }
-
-    if (this.output) {
-      this.output.appendLine(`telemetry/${eventName} ${JSON.stringify({props, measurements})}`);
     }
   }
 
@@ -63,10 +55,6 @@ class ExtensionTelemetry {
     if (this.reporter) {
       this.reporter.sendTelemetryException(error, props, measurements);
     }
-
-    if (this.output) {
-      this.output.appendLine(`telemetry/${error} ${JSON.stringify({props, measurements})}`);
-    }
   }
 
   public obfuscate(data: string): string {
@@ -76,10 +64,6 @@ class ExtensionTelemetry {
   public async dispose(): Promise<void> {
     if (this.reporter) {
       await this.reporter.dispose();
-    }
-
-    if (this.output) {
-      await this.output.dispose();
     }
   }
 }
