033d7580794ab1923069952d2ec583e56c22a7c9
Luis Mastrangelo <acuarica@users.noreply.github.com>
2022-12-22 15:26:12 +0100

    Simplify and remove dead code from `Validator` (#291)
    * refactor: simplify validators
    
    * refactor: simplify validator a bit more
    
    * refactor: refine constants scope
    
    * refactor: inline options array after MB suggestion

--- src/validators/UrlValidator.ts
+++ src/validators/UrlValidator.ts
@@ -1,8 +1,12 @@
 // Copyright (c) Consensys Software Inc. All rights reserved.
 // Licensed under the MIT license.
 
-import {Constants} from '../Constants';
 import {Validator} from './validator';
+
+const PORT_RE =
+  /^([1-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|[1-8][0-9]{3}|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9]|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/;
+
+export const INVALID_PORT_MSG = 'Invalid port.';
 
 export namespace UrlValidator {
   export function validateHostUrl(url: string): string | null {
@@ -12,6 +16,6 @@ export namespace UrlValidator {
   }
 
   export function validatePort(port: string | number): string | null {
-    return `${port}`.match(Constants.validationRegexps.port) ? null : Constants.validationMessages.invalidPort;
+    return `${port}`.match(PORT_RE) ? null : INVALID_PORT_MSG;
   }
 }
